66e24a439b1f2a8e378bd625f8ae6e58a329f0f2
Saga- <danielorangerl@gmail.com>
2022-07-10 14:16:39 +0100

    02-js-tools

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,25 +1,23 @@
-const Pet = (props) => {
-  return (
-    React.createElement('div', {}, [
-      React.createElement('h1', {}, props.name),
-      React.createElement('h2', {}, props.animal),
-      React.createElement('h2', {}, props.breed)
-    ]
-  ));
-}
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import Pet from './Pet';
 
 const App = () => {
-  return React.createElement(
-    'div',
-    {},
-    [
-      React.createElement('h1', { id: 'brand' }, 'Adopt Me!'),
-      React.createElement(Pet, { name: 'Luna', animal: 'Dog', breed: 'Havanese' }),
-      React.createElement(Pet, { name: 'Pepper', animal: 'Bird', breed: 'Cockatiel' }),
-      React.createElement(Pet, { name: 'Doink', animal: 'Cat', breed: 'Mix' })
-    ]
-  );
-}
+  return React.createElement('div', {}, [
+    React.createElement('h1', { id: 'brand' }, 'Adopt Me!'),
+    React.createElement(Pet, {
+      name: 'Luna',
+      animal: 'Dog',
+      breed: 'Havanese',
+    }),
+    React.createElement(Pet, {
+      name: 'Pepper',
+      animal: 'Bird',
+      breed: 'Cockatiel',
+    }),
+    React.createElement(Pet, { name: 'Doink', animal: 'Cat', breed: 'Mix' }),
+  ]);
+};
 const container = document.getElementById('root');
-const root = ReactDOM.createRoot(container);
+const root = createRoot(container);
 root.render(React.createElement(App));
